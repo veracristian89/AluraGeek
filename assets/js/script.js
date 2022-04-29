aad731aaa381e74dc27ea2602dcfb3f110d08e4c
Cristian <veracristian89@gmail.com>
2022-04-29 12:30:33 -0300

    se agrega script.js y se comienza con logica de validacion para form login

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const entrarBtn = document.querySelector("[data-entrar-boton]")
+
+function validandoMail(){
+    const mailImput = document.querySelector("[data-mail-input]")
+}
+function validandoClave(){
+    const claveInput = document.querySelector("[data-clave-input]")
+}
+entrarBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    alert("click en boton entrar");
+});
