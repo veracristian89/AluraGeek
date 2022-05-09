06fd26b072360117f447a0b3879e4d2014f1d6c2
Cristian <veracristian89@gmail.com>
2022-05-09 11:50:10 -0300

    se agrega logica para drag and dropen nuevo.js

--- assets/js/nuevo.js
+++ assets/js/nuevo.js
@@ -1,44 +1,28 @@
-function dropHandler(ev) {
-    console.log('Fichero(s) arrastrados');
-  
-    // Evitar el comportamiendo por defecto (Evitar que el fichero se abra/ejecute)
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      // Usar la interfaz DataTransferItemList para acceder a el/los archivos)
-      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-        // Si los elementos arrastrados no son ficheros, rechazarlos
-        if (ev.dataTransfer.items[i].kind === 'file') {
-          var file = ev.dataTransfer.items[i].getAsFile();
-          console.log('... file[' + i + '].name = ' + file.name);
-        }
-      }
-    } else {
-      // Usar la interfaz DataTransfer para acceder a el/los archivos
-      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-      }
-    }
-  
-    // Pasar el evento a removeDragData para limpiar
-    removeDragData(ev)
-  }
+const initApp = () => {
+    const dropArea = document.querySelector(".nuevo-producto__arrastrar-imagen");
+    const active = () => dropArea.classList.add("borde-azul");
+    const inactive = () => dropArea.classList.remove("borde-azul");
+    const prevents = (e) => e.preventDefault();
 
-  function dragOverHandler(ev) {
-    console.log('File(s) in drop zone');
-  
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-  }
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(evtName => {
+        dropArea.addEventListener(evtName, prevents);
+    });
 
-  function removeDragData(ev) {
-    console.log('Removing drag data')
-  
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to remove the drag data
-      ev.dataTransfer.items.clear();
-    } else {
-      // Use DataTransfer interface to remove the drag data
-      ev.dataTransfer.clearData();
-    }
-  }
+    ['dragenter', 'dragover'].forEach(evtName => {
+        dropArea.addEventListener(evtName, active);
+    });
+
+    ['dragleave', 'drop'].forEach(evtName => {
+        dropArea.addEventListener(evtName, inactive);
+    });
+
+    dropArea.addEventListener("drop", handleDrop);
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const handleDrop = (e) => {
+    const dt = e.dataTransfer;  
+    const files = dt.files;
+    console.log(files);
+}
