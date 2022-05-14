7c8f1708e1061cbcf9ac92187520ec5f4f8b7063
Cristian <veracristian89@gmail.com>
2022-05-13 23:55:17 -0300

    se realiza fetch en productos similares

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,12 +1,18 @@
 import { listaProductos } from "../services/product-services.js";
+import { tarjetaProducto } from "./products-controller.js";
 
 const url = new URL(window.location);
 const id = url.searchParams.get("id");
+const category = url.searchParams.get("category");
+console.log(category);
 
 const producto = (id) => {
+    //para seccion producto
     const productSection = document.querySelector(".producto");
     const divImagen = document.createElement("div");
-    const divContenido =document.createElement("div")
+    const divContenido =document.createElement("div");
+    //para seccion similares
+    const similares = document.querySelector("#similares")
     listaProductos().then((data) => {
         data.forEach(product => {
             if (product.id == id){
@@ -14,14 +20,21 @@ const producto = (id) => {
                 divContenido.innerHTML = `<h2 class="producto__titulo">${product.titulo}</h2>
                 <p class="producto__precio">$${product.precio}</p>
                 <p class="producto__descripcion">${product.descripcion}</p>`
-                divImagen.classList.add('producto__img-contenedor')
-                divContenido.classList.add('producto__contenido')
+                divImagen.classList.add('producto__img-contenedor');
+                divContenido.classList.add('producto__contenido');
                 productSection.appendChild(divImagen);
                 productSection.appendChild(divContenido);
+            }
+            if(product.categoria == category && product.id != id){
+                 
+                similares.appendChild(tarjetaProducto(product.imagen, product.titulo, product.precio, product.id, product.categoria));
+                
             }
         });
     })
 }
+producto(id);
+
+  
 
 
-producto(id)
--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -4,29 +4,29 @@ const starWars = document.querySelector('.star-wars');
 const consolas = document.querySelector('.consolas');
 const diversos = document.querySelector('.diversos');
 
-const tarjetaProducto = (imagen, titulo, precio, id) => {
+export const tarjetaProducto = (imagen, titulo, precio, id, categoria) => {
     const tarjeta = document.createElement('div');
     const contenidoDiv = 
     `<img src="${imagen}" alt="${titulo}" class="caja__img">
     <p class="caja__descripcion">${titulo}</p>
     <p class="caja__precio">$ ${precio}</p>
-    <a href="producto.html?id=${id}" class="caja__link">Ver producto</a>`;
+    <a href="producto.html?id=${id}&category=${categoria}" class="caja__link">Ver producto</a>`;
     tarjeta.innerHTML = contenidoDiv;
     tarjeta.classList.add("caja")
     return tarjeta;
-    
 }
 
 listaProductos().then((data) => {
     data.forEach(producto => {
         if(producto.categoria == "Star Wars"){
-            starWars.appendChild(tarjetaProducto(producto.imagen, producto.titulo, producto.precio, producto.id));
+            starWars.appendChild(tarjetaProducto(producto.imagen, producto.titulo, producto.precio, producto.id, producto.categoria));
         }
         if (producto.categoria == "Consolas"){
-            consolas.appendChild(tarjetaProducto(producto.imagen, producto.titulo, producto.precio, producto.id));
+            consolas.appendChild(tarjetaProducto(producto.imagen, producto.titulo, producto.precio, producto.id, producto.categoria));
         }
         if (producto.categoria == "Diversos"){
-            diversos.appendChild(tarjetaProducto(producto.imagen, producto.titulo, producto.precio, producto.id));
+            diversos.appendChild(tarjetaProducto(producto.imagen, producto.titulo, producto.precio, producto.id, producto.categoria));
         }
     });
 })
+
