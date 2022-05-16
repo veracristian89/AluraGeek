985d9a9866388a35127550427f51c989c634328a
Cristian <veracristian89@gmail.com>
2022-05-16 12:13:03 -0300

    se cambia boton login por salir en administrador.htm y se agrega funcionalidad

--- assets/js/administrador.js
+++ assets/js/administrador.js
@@ -1,6 +1,13 @@
 const administradorBtn = document.querySelector("[data-administrador-btn]");
+const salirBtn = document.querySelector("[data-logout-btn]")
 
 administradorBtn.addEventListener("click", (event)=>{
     event.preventDefault();
-    window.location = "nuevo.html"
+    window.location = "nuevo.html";
+})
+
+salirBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    window.location = "index.html";
+
 })
