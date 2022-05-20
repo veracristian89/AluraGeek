02807660471657cdc0037e3e74ded293a00e10a4
Cristian <veracristian89@gmail.com>
2022-05-20 12:10:36 -0300

    se agrega boton search responsive

--- services/product-services.js
+++ services/product-services.js
@@ -1,3 +1,2 @@
-export const listaProductos = () => fetch('https://veracristian89.github.io/AluraGeekDB/db/products.json')
-    .then((response) => response.json());
+export const listaProductos = () => fetch('https://veracristian89.github.io/AluraGeekDB/db/products.json').then((response) => response.json());
 
