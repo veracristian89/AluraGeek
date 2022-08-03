d02d7bab08199ca0dae142347e606bd3de4b44aa
Cristian <veracristian89@gmail.com>
2022-08-03 11:09:09 -0300

    se automatiza porcentaje de promocion dependiendo del mes

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,14 +2,22 @@ let fecha = new Date();
 let mes = fecha.getMonth();
 let meses = ["Enero", "Febrero", "Marzo", "Abril","Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-function elegirMes(array, indice){
-    return array[indice];
+function promocion(){
+    let porcentaje;
+    if(mes == 11){
+        porcentaje = "50%";
+    } else if(mes >= 7 & mes < 11){
+        porcentaje = "40%";
+    } else {
+        porcentaje = "33%";
+    }
+    return porcentaje;
 }
 
 let portadaContenedor = document.querySelector(".portada__contenido");
 
-portadaContenedor.innerHTML = `<h2 class="portada__titulo">${elegirMes(meses, mes)} Promocional</h2>
-<p class="portada__descripcion">Productos selecionados con 33% de descuento</p>
+portadaContenedor.innerHTML = `<h2 class="portada__titulo">${meses[mes]} Promocional</h2>
+<p class="portada__descripcion">Productos selecionados con ${promocion()} de descuento</p>
 <form action="#consolas">
     <button class="portada__boton">Ver Consolas</button>
 </form>`
